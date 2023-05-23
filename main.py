506235780884d3f6b30e6f3e51d11def97e5aaf1
Abhishek Kapoor <kapoorkk1980@gmail.com>
2023-05-23 15:19:59 +0530

    Initial Commit

---
+++ main.py
@@ -0,0 +1,112 @@
+import json
+from moviepy.editor import VideoFileClip, AudioFileClip
+from flask import Flask, render_template, request, send_from_directory
+import re
+from pytube import YouTube
+import datetime
+
+app = Flask(__name__)
+
+
+def get_youtube_video_id(url):
+    video_id_regex = r"(?:youtu\.be\/|youtube\.com\/(?:embed\/|v\/|watch\?v=|watch\?feature=player_embedded&v=))([\w-]{11})"
+    match = re.search(video_id_regex, url)
+    if match:
+        return match.group(1)
+    else:
+        return None
+
+def clean_filename(name):
+        """Ensures each file name does not contain forbidden characters and is within the character limit"""
+        # For some reason the file system (Windows at least) is having trouble saving files that are over 180ish
+        # characters.  I'm not sure why this is, as the file name limit should be around 240. But either way, this
+        # method has been adapted to work with the results that I am consistently getting.
+        forbidden_chars = '"*\\/\'.|?:<>'
+        filename = (''.join([x if x not in forbidden_chars else '#' for x in name])).replace('  ', ' ').strip()
+        if len(filename) >= 176:
+            filename = filename[:170] + '...'
+        return filename 
+
+
+def download_youtube_video_audio(url, filetype, extension, res):
+    yt = YouTube(url)
+    if filetype == "video":
+        aud_vid = yt.streams.filter(type = filetype, resolution=res, progressive=False, only_video=True).first()
+        dir = "media\\video\\"
+    elif filetype == "audio":
+        aud_vid = yt.streams.filter(type = filetype, bitrate=res, progressive=False, only_audio=True).first()
+        dir = "media\\audio\\"
+    time = datetime.datetime.now().strftime("%d-%m-%Y-%H-%M-%S")
+    filename = f"{clean_filename(yt.title)}-{time}.{extension}"
+    path = dir+filename
+    aud_vid.download(output_path=dir, filename=filename)
+    return [dir, filename, path]
+
+
+def max_bitrate(url):
+    yt = YouTube(url)
+    aud = yt.streams.filter(type="audio")
+    return sorted(list({i.abr for i in aud}), key=lambda x:int(x[:-4]), reverse=True)[0]
+
+
+def mergeAudVid(audio_path, video_path, filename):
+    video = VideoFileClip(video_path)
+    audio = AudioFileClip(audio_path)
+
+    dir = f"media\\mergedAudVid\\"
+    path = dir+filename
+
+    video = video.set_audio(audio)
+    video.write_videofile(path)
+    return [dir, filename, path]
+
+
+
+@app.route("/", methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        url = request.form.get("url")
+        filetype = request.form.get("type")
+        extension = request.form.get("extension")
+        res = request.form.get("res")
+
+        # check url
+        video_id = get_youtube_video_id(url)
+        if video_id:
+            if filetype=="video":
+                # vid_path is [dir, filename, path]
+                vid_path = download_youtube_video_audio(url, filetype, extension, res)
+                aud_path = download_youtube_video_audio(url, "audio", "mp3", max_bitrate(url))
+                output_path = mergeAudVid(aud_path[2], vid_path[2], vid_path[1])
+                return send_from_directory(output_path[0], output_path[1], as_attachment=True)
+
+            elif filetype == "audio":
+                path = download_youtube_video_audio(url, filetype, extension, res)
+                return send_from_directory(path[0], path[1], as_attachment=True)
+    return render_template("index.html")
+
+@app.route("/resolution/", methods=["POST"])
+def resolution():
+    if request.method == "POST":
+        url = request.json.get("url")
+        fileType = request.json.get("type")
+        # extension = request.json.get("extension")
+
+        try:
+            yt = YouTube(url)
+            vidStream = yt.streams.filter(type=fileType, progressive=False)
+            if fileType == "video":
+                vidres = sorted(list({i.resolution for i in vidStream}), key=lambda x:int(x[:-1]))
+                print(vidres)
+                vidReturnJSON = json.dumps(vidres)
+            elif fileType == "audio":
+                vidabr = sorted(list({i.abr for i in vidStream}), key=lambda x:int(x[:-4]))
+                vidReturnJSON = json.dumps(vidabr)
+            return vidReturnJSON
+        except:
+            return "[\"inexcept\"]"
+    else:
+        return "[]"
+
+if __name__=="__main__":
+    app.run(debug=True)
---
+++ static/js/script.js
@@ -0,0 +1,94 @@
+
+document.getElementById("typediv").display = "none";
+document.getElementById("extdiv").display = "none";
+
+
+document.getElementById("url").onchange = ()=>{
+    document.getElementById("typediv").display = "block";
+}
+
+
+// function for showing extension
+const loadExt = (event)=>{
+    event.preventDefault()
+    let fileType = document.getElementById("type").value;
+    if (fileType=="video") {
+        document.getElementById("extension").length = 0;
+        document.getElementById("extdiv").display = "block";
+        
+        extensions = [
+            "mp4",
+            "webm",
+            "3gpp"
+        ]
+        for (let key in extensions) {
+            var ext = document.getElementById("extension")
+            var opt = document.createElement("option");
+            opt.value = extensions[key];
+            opt.innerHTML = extensions[key];
+            ext.append(opt)
+        }
+    } else if (fileType=="audio") {
+        document.getElementById("extension").length = 0;
+        document.getElementById("extdiv").display = "block";
+        
+        extensions = [
+            "mp3",
+            "wav"
+        ]
+        for (let key in extensions) {
+            var ext = document.getElementById("extension")
+            var opt = document.createElement("option");
+            opt.value = extensions[key];
+            opt.innerHTML = extensions[key];
+            ext.append(opt)
+        }
+    }
+}
+
+// fetch function for showing extensions
+const loadRes =  async (event) => {
+    event.preventDefault()
+    document.getElementById("download").disabled=true
+    document.getElementById("resdiv").display = "none";
+    document.getElementById("res").length = 0;
+    // const csrftoken = document.querySelector('[name=csrfmiddlewaretoken]').value;
+    let formData = {
+        url: document.querySelector('[name=url]').value,
+        type: document.querySelector('[name=type]').value,
+        ext: document.querySelector('[name=extension]').value
+    }
+    console.log(formData)
+
+    const response = await fetch('/resolution/', {
+        "method": "POST",
+        "headers": {"Content-Type": "application/json"},
+        "body": JSON.stringify(formData)
+    })
+    const jsonData = await response.json();
+    console.log(jsonData)
+
+    if (Object.keys(jsonData).length==0) {
+        document.getElementById("urlVal").innerHTML = "Something wrong with url please check it again!"
+    } else {
+        document.getElementById("res").length = 0;
+        document.getElementById("resdiv").display = "block";
+        if (document.getElementById("type").value=="video"){
+            document.getElementById("resLabel").innerHTML = "Resolution";
+        } else {
+            document.getElementById("resLabel").innerHTML = "Bitrate";
+        }
+        
+        for (let key in jsonData) {
+            var resType = document.getElementById("res")
+            var opt = document.createElement("option");
+            opt.value = jsonData[key];
+            opt.innerHTML = jsonData[key];
+            resType.append(opt)
+        }
+        document.getElementById("download").disabled=false
+    }
+}
+
+document.getElementById("type").addEventListener("click", loadExt);
+document.getElementById("type").addEventListener("click", loadRes);
